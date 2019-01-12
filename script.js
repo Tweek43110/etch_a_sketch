af392de89d406509c1ab52d021341d52c418a6d9
Alex Coe <alex.r.coe@gmail.com>
2019-01-12 14:34:30 -0600

    added resetGrid function

--- script.js
+++ script.js
@@ -1,28 +1,63 @@
 let container = document.getElementById('container');
 
-function addDiv(){
-    let newDiv = document.createElement('div');
-    newDiv.setAttribute('class', 'grid');
-    container.appendChild(newDiv);
-}
+function createGrid() {
+    let i = 0;
+    //Default grid size
+    let j = 64;
 
-//create 70 div elements
-for (let i = 0; i < 70; i++) {
-    addDiv();
-}
-
-//Create variable for grid class
-let grid = document.getElementsByClassName('grid');
-
-//Apply event listener to all grid elements after they have been created
-for (let i = 0; i<70; i++) {
-    console.log(grid[i]);
-    grid[i].addEventListener('mouseover', drawColor)
+    function addDiv(){
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute('class', 'grid');
+        container.appendChild(newDiv);
+    }
+    //create 64 div elements
+    for (i = 0; i < j; i++) {
+        addDiv();
     }
 
-let color = document.getElementsByClassName('grid');
+    //Create variable for grid class
+    let grid = document.getElementsByClassName('grid');
 
-function drawColor(e) {
-    console.log(e);
-    e.toElement.style.backgroundColor = "grey";
+    //Apply event listener to all grid elements after they have been created
+    for (let i = 0; i < 64; i++) {
+        grid[i].addEventListener('mouseover', drawColor)
+        }
+
+    function drawColor(e) {
+        //Was getting error with grid.style.backgroundColor being undefined
+        e.toElement.style.backgroundColor = "grey";
+    }
+
+    //Button instructions
+    let resetButton = document.getElementById('reset');
+    resetButton.addEventListener('click', resetGrid);
+}
+createGrid();
+
+function resetGrid() {
+    let newGrid = prompt('What size should the new grid be? (10-64)');
+    
+    function removeElements() {
+        //Taken from here: https://stackoverflow.com/questions/3387427/remove-element-by-id
+        Element.prototype.remove = function() {
+            this.parentElement.removeChild(this);
+        }
+        NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+            for(var i = this.length - 1; i >= 0; i--) {
+                if(this[i] && this[i].parentElement) {
+                    this[i].parentElement.removeChild(this[i]);
+                }
+            }
+        }
+        document.getElementsByClassName('grid').remove();
+    }
+    
+    if (newGrid < 10 || newGrid > 64) {
+        newGrid = prompt("Please enter a value between 10 and 64");
+    } else if (newGrid >= 10 || newGrid <= 64) {
+        console.log(newGrid);
+        let j = newGrid;
+        removeElements(); 
+        createGrid();   
+    }
 }
