52a43f9840e69920ba097b317a42f3188d4719b8
Alex Coe <alex.r.coe@gmail.com>
2019-01-12 13:42:26 -0600

    Added hover draw listener

--- script.js
+++ script.js
@@ -6,19 +6,23 @@ function addDiv(){
     container.appendChild(newDiv);
 }
 
-//creates 16 div elements
-var i;
-for (i = 0; i < 16; i++) {
-    addDiv(i);
+//create 70 div elements
+for (let i = 0; i < 70; i++) {
+    addDiv();
 }
 
-/* Hold to figure out why this doesnt work
-let grid = document.getElementsByClassName('grid').addEventListener('mouseenter', drawColor);
-function drawColor() {
+//Create variable for grid class
+let grid = document.getElementsByClassName('grid');
+
+//Apply event listener to all grid elements after they have been created
+for (let i = 0; i<70; i++) {
+    console.log(grid[i]);
+    grid[i].addEventListener('mouseover', drawColor)
+    }
+
+let color = document.getElementsByClassName('grid');
+
+function drawColor(e) {
     console.log(e);
+    e.toElement.style.backgroundColor = "grey";
 }
-*/
-
-
-
-
